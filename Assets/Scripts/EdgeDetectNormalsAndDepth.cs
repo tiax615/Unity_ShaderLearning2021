1a4e7c455be87a04130d22db9c405a4eccf37012
caidu <cd@ahucd.cn>
2021-03-20 17:49:24 +0800

    temp: 20210320

--- Assets/Scripts/EdgeDetectNormalsAndDepth.cs
+++ Assets/Scripts/EdgeDetectNormalsAndDepth.cs
@@ -2,6 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EdgeDetectNormalsAndDepth : PostEffectsBase{
-    
+public class EdgeDetectNormalsAndDepth : PostEffectsBase
+{
+    public Shader edgeDetectShader;
+    private Material edgeDetectMaterial = null;
+    public Material material
+    {
+        get
+        {
+            edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+            return edgeDetectMaterial;
+        }
+    }
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
+    public Color edgeColor = Color.black;
+    public Color backgroundColor = Color.white;
+    public float sampleDistance = 1.0f; // 采样距离。视觉上看，值越大描边越宽
+    public float sensitivityDepth = 1.0f; // 邻域深度灵敏度
+    public float sensitivityNormals = 1.0f; // 邻域法线灵敏度
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode|=DepthTextureMode.DepthNormals;
+    }
 }
